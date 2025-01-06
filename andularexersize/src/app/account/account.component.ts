197cdd84fdc1de87bab5010507a46e2d815bd040
Jafarhajeer1 <jafar.hajeer@najah.edu>
2025-01-06 11:07:38 +0200

    ngClass + ngStyle

--- andularexersize/src/app/account/account.component.ts
+++ andularexersize/src/app/account/account.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+//import { NgClass } from '@angular/common';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-account',
-  imports: [],
+  imports: [ NgStyle ],
   templateUrl: './account.component.html',
   styleUrl: './account.component.css'
 })
-export class AccountComponent {
+export class AccountComponent implements OnInit{
+  myStyles:Record<string,string>={}
 
+  setupStyles(){
+    this.myStyles = {
+      'background-color':'#666666',
+      'color':'#ffffff'
+    }
+  }
+
+  ngOnInit(): void {
+    this.setupStyles();
+  }
 }
